0a75c6208cd6cb1186baa5dd2114aeb7f1c1aee2
MXJXN <modulations@gmail.com>
2020-02-10 10:14:01 -0500

    updating for debian defaults

--- keybindings.el
+++ keybindings.el
@@ -19,8 +19,6 @@
   "a" 'sclang-show-method-args)
 (spacemacs/set-leader-keys-for-major-mode 'sclang-mode
   "w" 'sclang-switch-to-workspace)
-(spacemacs/set-leader-keys-for-major-mode 'sclang-mode
-  "!" 'sclang-reset-spacemacs-conf)
 
 ;; ==============
 ;;   prefixes
--- packages.el
+++ packages.el
@@ -4,8 +4,8 @@
   (use-package sclang
     :init
     (sclang-create-spacemacs-conf-file)
-    :config
-    (progn
-      (setenv "PATH" (concat (getenv "PATH") ":/Applications/SuperCollider.app/Contents/MacOS"))
-      (setq exec-path (append exec-path '("/Applications/SuperCollider.app/Contents/MacOS" )))
-      (setq sclang-library-configuration-file "~/Library/Application Support/SuperCollider/spacemacs_conf.yaml"))))
+    ;; :config
+    ;; (progn
+         ;;TODO check if sclang and scsynth are present
+    ;; )
+    ))
